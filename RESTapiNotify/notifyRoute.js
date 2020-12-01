50db3f50a9b22fc8e856bbfe323173d6ad6a47aa
Gonzalo Damian Molina <gmolina@seidoranalytics.com>
2020-12-01 00:43:35 -0300

    funcionando envío de mails

--- RESTapiNotify/notifyRoute.js
+++ RESTapiNotify/notifyRoute.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const notify = express();
+const GmailAPI = require('../GMailAPIClient')
+const GmailAPIInstance = new GmailAPI();
 
 notify.post('/notify', (req, res) => {
     let artistId = parseInt(req.body.artistId);
     try{ 
         let artist = req.unQify.getArtistById(artistId)
         let interestedUsers = artist.getSubscribers()
-        //req.unQify.save();
+        interestedUsers.forEach(user => GmailAPIInstance.send_mail(
+            req.body.subject,req.body.message,{email: user},{email: 'gonza.molinad@gmail.com'}
+            )).then( (gmailResponse) => {
+                console.log("Mail enviado!");
+                console.log(gmailResponse);
+              }).catch( (error) => {
+                console.error("Algo salió mal");
+                console.error(error);
+              })
+        req.unQify.save();
         console.log(res)
         res.status(200).json(interestedUsers);
     }catch(e){
